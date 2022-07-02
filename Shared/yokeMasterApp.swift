6443819918f80bf8fbc4b860aa9aede39fc72b93
Nick Viamin <viaminnick87@gmail.com>
2022-07-01 23:04:24 -0400

    Initial Commit

---
+++ Shared/yokeMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  yokeMasterApp.swift
+//  Shared
+//
+//  Created by Nick Viamin on 7/1/22.
+//
+
+import SwiftUI
+
+@main
+struct yokeMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
